4d198059694033c0d3b41d1f0da0acb033704b69
jiho.lee <search5@gmail.com>
2014-03-07 19:26:01 +0900

    일부 기능 개선

--- proper/__init__.py
+++ proper/__init__.py
@@ -7,6 +7,7 @@ import os
 import util
 import json
 import re
+import math
 from openpyxl import Workbook
 
 from flask import Flask, abort, g, render_template, request, redirect, url_for, send_file
@@ -16,6 +17,7 @@ from dateutil.parser import parse
 #from werkzeug.utils import secure_filename
 from cStringIO import StringIO
 import datetime
+from dateutil.relativedelta import relativedelta
 
 
 app = Flask(__name__)
@@ -25,14 +27,19 @@ db = SQLAlchemy(app)
 
 from proper.models import User, Dept, ServiceRequest, Asset, AssetHistory
 
-def connect_db():
-    pass
-#    return MySQLdb.connect(host="localhost", user="sc_it", passwd="1234", db="sc_it", use_unicode=True, charset="utf8")
+def oldYears(dob):
+    today = datetime.date.today()
+
+    if today.month < dob.month or \
+      (today.month == dob.month and today.day < dob.day):
+        return today.year - dob.year - 1
+    else:
+        return today.year - dob.year
+
 
 
 @app.before_request
 def before_request():
-    g.db = connect_db()
     remote_addr = request.remote_addr
 
     remote_first_addr = remote_addr[:remote_addr.rfind(".") + 1]
@@ -182,7 +189,7 @@ def pc_asset_update(asset_code):
     changed_data = record - user_input_dict
 
     # 변경내역이 None이 아니고 하나 이상일 경우 수행하게 한다.
-    if (changed_data != None) and (len(changed_data) > 0):
+    if changed_data != None:
         for item in changed_data:
             # 히스토리 레코드 생성
             asset_history = AssetHistory(asset_code, **item)
@@ -341,7 +348,7 @@ def notebook_asset_update(asset_code):
     changed_data = record - user_input_dict
 
     # 변경내역이 None이 아니고 하나 이상일 경우 수행하게 한다.
-    if (changed_data != None) and (len(changed_data) > 0):
+    if changed_data != None:
         for item in changed_data:
             # 히스토리 레코드 생성
             asset_history = AssetHistory(asset_code, **item)
@@ -444,7 +451,7 @@ def monitor_asset_update(asset_code):
     changed_data = record - user_input_dict
 
     # 변경내역이 None이 아니고 하나 이상일 경우 수행하게 한다.
-    if (changed_data != None) and (len(changed_data) > 0):
+    if changed_data != None:
         for item in changed_data:
             # 히스토리 레코드 생성
             asset_history = AssetHistory(asset_code, **item)
@@ -737,7 +744,10 @@ def user_asset_excel():
                     value = asset_type[value]
                 elif db_column == "make_date":
                     if value:
-                        tmp_date = value.split(".")
+                        tmp_date = re.split("[\.\-]", value)
+                        # tmp_date의 월이 12이상인 경우 12로 강제 조정한다.
+                        if tmp_date[1] > 12:
+                            tmp_date[1] = 12
                         value = datetime.date(int(tmp_date[0]), int(tmp_date[1]), 1)
 
                 if column["title"] == u"제조년월":
@@ -750,8 +760,11 @@ def user_asset_excel():
                     #if value and "." in value:
                     #    value = value.split(".")[0]
                     today = datetime.datetime.today().date()
-                    today_delta = today - value
-                    value = today_delta.days / (365.25)
+                    if value:
+                        #today_delta = today - value
+                        value = oldYears(value) + 1
+                    else:
+                        value = 0
 
                 cell.value = value or ''
 
