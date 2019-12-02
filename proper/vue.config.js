8e62ff0ecb947d7d19838e104dfe5a10096021a7
이지호 <search5@gmail.com>
2019-12-02 21:23:01 +0900

    기본 API 환경 구성

--- proper/vue.config.js
+++ proper/vue.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   'transpileDependencies': [
     'vuetify'
-  ]
+  ],
+  devServer: {
+    proxy: { // proxyTable 설정
+      '/api': {
+        target: 'http://localhost:5000',
+        publicPath: {
+          '^/api': ''
+        }
+      }
+    }
+  }
 }
