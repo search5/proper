ec45acd302a72d7a1fdde208b72e3ecd454c70ea
이지호 <search5@gmail.com>
2019-12-02 21:38:24 +0900

    미리 추가해둔 VueRouter가 Vuetify에서 동작하도록 수정

--- proper/src/App.vue
+++ proper/src/App.vue
@@ -38,20 +38,16 @@
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
   name: 'App',
 
-  components: {
-    HelloWorld
-  },
+  components: {},
 
   data: () => ({
     //
